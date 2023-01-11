332dd9b40440d29985c9cede8c8ac40ca91c325a
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-11 18:29:26 -0500

    Modified the animation by learning an extension of the Transition component, turning it from changing colors to fading in and out. This was accomplished by styling fade-enter-to and fade-enter-from whenever the toggle button is clicked.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@
 
 <template>
   <main>
-    <Transition name="color">
+    <Transition name="fade">
       <h1 v-if="showText">Hello World</h1>
     </Transition>
     <button @click="showText = !showText">
@@ -17,14 +17,25 @@
 
 <style scoped>
 
-.color-enter-from {
-  background-color: yellow;
+.fade-enter-from {
+  opacity: 0;
 }
-.color-enter-to {
-  background-color: blue;
+.fade-enter-to {
+  opacity: 1;
 }
 
-  .color-enter-active {
-    transition: all 5s ease;
-  }
+.fade-enter-active {
+  transition: all 1s ease;
+}
+
+.fade-leave-from {
+  opacity: 1;
+}
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-leave-active {
+  transition: all 1s ease;
+}
 </style>
