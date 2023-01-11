0d3cd510e1667c83dc6d7a980c8e489b150ddde4
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-11 18:13:33 -0500

    This commit is to establish the initial boilerplate using @vue-latest to jumpstart our application

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+  import { ref } from 'vue'
+
+  const showText = ref(true)
+</script>
+
+<template>
+  <main>
+    <h1 v-if="showText">Hello World</h1>
+    <button @click="showText = !showText">
+      Toggle
+    </button>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
