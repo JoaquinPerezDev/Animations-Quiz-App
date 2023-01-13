0cadd0bcb4e1467389a2c4379a99bee0b160d753
LucasPerezDev <LucasPerezDev@gmail.com>
2023-01-13 17:57:05 -0500

    Refactored this application to a simpler app that adds names to an invite list using @keypress handler. We specify the enter key so that when the user enters a name into the text input, it'll use the addInvitee function to add the name and publish it to the list.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,18 @@
 <script setup>
   import { ref } from 'vue'
 
-  const showGreet = ref(true)
+  const names = ref(['Selena Gomez', 'Sandra Lopez'])
+  const nameInput = ref('');
+
+  const addInvitee = () => {
+    names.value.unshift(nameInput.value)
+    nameInput.value = ''
+  }
 </script>
 
 <template>
   <main>
+    <!--     
     <div class="container">
       <Transition name="fade">
         <h1 v-if="showGreet">Hello World</h1>
@@ -16,11 +23,27 @@
     <button @click="showGreet = !showGreet">
       Toggle
     </button>
+     -->
+  <div class="container">
+    <input 
+      type="text" 
+      placeholder="A person to invite..." 
+      v-model="nameInput"
+      @keypress.enter="addInvitee"
+    >
+    <ul>
+      <li v-for="name in names" :key="name">
+        {{ name }}
+      </li>
+    </ul>
+  </div>
+  
   </main>
+
 </template>
 
 <style scoped>
-
+/*
 .fade-enter-from {
   opacity: 0;
 }
@@ -53,5 +76,38 @@ h1 {
 
 button {
   margin-top: 50px;
+}
+*/
+
+.container {
+  max-width: 300px;
+  margin: 0 auto;
+}
+
+.container input {
+  width: 100%;
+  border-radius: 5px;
+  border: 1px solid rgba(128,128,128,0.13);
+  padding: 10px;
+  margin-bottom: 20px;
+  box-shadow: 1px 1px 10px black;
+}
+
+ul {
+  display: inline;
+  list-style: none;
+  margin: 0px;
+  padding: 0px; 
+}
+
+li {
+  list-style: none;
+  width: 300px;
+  border-radius: 5px;
+  padding: 5px 10px;
+  margin-top: 10px;
+  box-shadow: 1px 1px 10px black;
+  text-align: center;
+  cursor: pointer;
 }
 </style>
